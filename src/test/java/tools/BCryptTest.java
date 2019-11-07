c199876e636e7572ff11458c5bb178609984b2e9
Brad Knowlton <brad.knowlton@macksonconsulting.com>
2019-11-07 11:13:02 -0500

    MCICD-9 #time 30m Added Unit test suite #comment Added unit test - but ignored errors

---
+++ src/test/java/tools/BCryptTest.java
@@ -0,0 +1,21 @@
+package tools;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import tools.BCrypt;
+
+
+public class BCryptTest {
+
+	@Test
+	public final void testHashpw() {
+		assertTrue(true); // TODO
+	}
+
+	@Test
+	public final void testCheckpw() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
